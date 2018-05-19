95d9e37325db380d9d52112aad281eaf4c0f19ef
mjw <msql518@gmail.com>
2018-05-20 02:21:40 +0800

    modify install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 cpath=$(cd "$(dirname "$0")";pwd) 
 cd ~
-rm -r .vim .vimrc .tmux .tmux.conf .zshrc 
+rm -rf .vim .vimrc .tmux .tmux.conf .zshrc 
+
 cd ${cpath}
-cp -r .vim .vimrc .tmux .tmux.conf .zshrc ~
+cp -rf .vim .vimrc .tmux .tmux.conf .zshrc .ycm_extra_conf.py ycm_build ~
+
+# # YCM
+# sudo apt-get update --fix-missing
+# sudo apt-get install build-essential cmake3 --fix-missing
+# sudo apt-get install python-dev python3-dev --fix-missing
+# cd ~/.vim/bundle/YouCompleteMe
+# ./install.py --clang-completer
